ebc803a4327b26d7c20cc0a15229bcada5de8045
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2021-04-22 12:39:07 -0300

    feat: user id page

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,18 +1,61 @@
-import React from 'react'
-import { SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useCallback, useState } from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native'
+
+import Button from '../components/Button'
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
 
 function UserIdentification() {
+  const [isFocused, setIsFocused] = useState(false)
+  const [isFilled, setIsFilled] = useState(false)
+  const [name, setName] = useState<string>()
+
+  const handleInputFocus = () => setIsFocused(true)
+  const handleInputBlur = () => {
+    setIsFocused(false)
+    setIsFilled(!!name)
+  }
+  const handleInputChange = useCallback((value: string) => {
+    setIsFilled(!!value)
+    setName(value)
+  }, [])
+
   return (
     <SafeAreaView style={style.container}>
-      <View style={style.content}>
-        <View style={style.form}>
-          <Text style={style.emoji}>😃</Text>
-          <Text style={style.title}>Como podemos {'\n'} chamar você?</Text>
-          <TextInput style={style.input} />
+      <KeyboardAvoidingView
+        style={style.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <View style={style.content}>
+          <View style={style.form}>
+            <View style={style.header}>
+              <Text style={style.emoji}>{isFilled ? '😃' : '😄'}</Text>
+              <Text style={style.title}>Como podemos {'\n'} chamar você?</Text>
+            </View>
+            <TextInput
+              style={[
+                style.input,
+                (isFocused || isFilled) && { borderColor: colors.green },
+              ]}
+              placeholder='Digite seu nome'
+              onBlur={handleInputBlur}
+              onFocus={handleInputFocus}
+              onChange={() => handleInputChange}
+            />
+            <View style={style.footer}>
+              <Button title='Confirmar' activeOpacity={0.8} />
+            </View>
+          </View>
         </View>
-      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   )
 }
@@ -24,14 +67,23 @@ const style = StyleSheet.create({
     justifyContent: 'space-around',
     width: '100%',
   },
-  content: { flex: 1, width: '100%' },
+  content: {
+    flex: 1,
+    width: '100%',
+  },
   emoji: { fontSize: 44 },
+  footer: {
+    marginTop: 40,
+    paddingHorizontal: 20,
+    width: '100%',
+  },
   form: {
     alignItems: 'center',
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 54,
   },
+  header: { alignItems: 'center' },
   input: {
     borderBottomWidth: 1,
     borderColor: colors.gray,
