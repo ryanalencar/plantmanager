d27584a78b8ea0e0f86891c778f1e1abb483ccde
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2021-04-22 11:17:39 -0300

    refactor: wellcome page fonts

--- src/pages/Wellcome.tsx
+++ src/pages/Wellcome.tsx
@@ -6,26 +6,30 @@ import {
   StyleSheet,
   TouchableOpacity,
   Dimensions,
+  View,
 } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
 import watering from '../assets/watering.png'
 import colors from '../styles/colors'
+import fonts from '../styles/fonts'
 
 function Wellcome(): any {
   return (
     <SafeAreaView style={style.container}>
-      <Text style={style.title}>
-        Gerencie {'\n'} suas plantas {'\n'}de forma fácil
-      </Text>
-      <Image source={watering} style={style.image} resizeMode='contain' />
-      <Text style={style.subtitle}>
-        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-        sempre que precisar.
-      </Text>
-      <TouchableOpacity activeOpacity={0.8} style={style.button}>
-        <Feather name='chevron-right' size={32} color='#fff' />
-      </TouchableOpacity>
+      <View style={style.wrapper}>
+        <Text style={style.title}>
+          Gerencie {'\n'} suas plantas de{'\n'} forma fácil
+        </Text>
+        <Image source={watering} style={style.image} resizeMode='contain' />
+        <Text style={style.subtitle}>
+          Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+          sempre que precisar.
+        </Text>
+        <TouchableOpacity activeOpacity={0.8} style={style.button}>
+          <Feather name='chevron-right' size={32} color='#fff' />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
@@ -41,25 +45,32 @@ const style = StyleSheet.create({
     width: 56,
   },
   container: {
-    alignItems: 'center',
     flex: 1,
-    justifyContent: 'space-around',
   },
   image: {
     height: Dimensions.get('window').width * 0.7,
   },
   subtitle: {
     color: colors.heading,
+    fontFamily: fonts.text,
     fontSize: 18,
     paddingHorizontal: 20,
     textAlign: 'center',
   },
   title: {
     color: colors.heading,
-    fontSize: 32,
+    fontFamily: fonts.heading,
+    fontSize: 28,
     fontWeight: 'bold',
+    lineHeight: 34,
     marginTop: 40,
     textAlign: 'center',
+  },
+  wrapper: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'space-around',
+    paddingHorizontal: 20,
   },
 })
 
